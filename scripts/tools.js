cab0f4383042c3603db56bd52378994843b863fb
RGBCube <33258743+TheFuryBumblebee@users.noreply.github.com>
2023-05-23 11:46:18 -0700

    Update tools.js

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,1 +1,5 @@
 const params = new URL(window.location.toLocaleString()).searchParams;
+let iframe = document.getElementById("tool");
+if (params.has("tool")) {
+    iframe.setAttribute("src", params.get("tool"));
+}
